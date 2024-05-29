147058c39394fb0fddcfb0ec1873ac8eb7f7a5e6
Keith Sharp <kms@passback.co.uk>
2024-05-29 13:57:15 +0100

    Initial Commit

---
+++ VacuumTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacuumTest
+//
+//  Created by Keith Sharp on 29/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacuumTest/VacuumTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacuumTestApp.swift
+//  VacuumTest
+//
+//  Created by Keith Sharp on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct VacuumTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
